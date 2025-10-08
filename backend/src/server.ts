97c15e5e886dcc7178efde6aa31d608f1c23e1ca
MateusMonteiroDV <mateusdosantosmonteiro12@gmail.com>
2025-10-08 08:29:46 -0300

    setup_authentication_from_jwt

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,12 +2,9 @@ import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
 import express from "express";
 
-console.log("USER_DATABASE_DEV:", process.env.USER_DATABASE_DEV);
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-
-app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello world");
---
+++ backend/src/utils/jwt.ts
@@ -0,0 +1,22 @@
+// utils/jwt.ts
+import jwt, { SignOptions, JwtPayload, Secret } from "jsonwebtoken";
+
+const SECRET: Secret = process.env.JWT_SECRET || "default-secret";
+
+export const signToken = (
+  payload: string | object | Buffer,
+  expiresIn: number | 1,
+): string => {
+  const options: SignOptions = { expiresIn };
+  return jwt.sign(payload, SECRET, options);
+};
+
+export const verifyToken = (token: string): JwtPayload => {
+  const decoded = jwt.verify(token, SECRET);
+
+  if (typeof decoded === "string") {
+    throw new Error("Invalid token payload: expected an object");
+  }
+
+  return decoded;
+};
